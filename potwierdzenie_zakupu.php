2d48cf0d8572167d66bc55c56d4849a3d90bfca2
JimboBajden <136314043+JimboBajden@users.noreply.github.com>
2024-05-23 20:53:21 +0200

    sigma

--- potwierdzenie_zakupu.php
+++ potwierdzenie_zakupu.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css4.css">
 </head>
 <body>
     <?php
@@ -11,31 +12,11 @@
         $connect=@new mysqli('localhost','root','','lamazon');
     ?>  
     <center><h1>wybierz metode płatności</h1></center>
-    <form action="zamowienia.php" method="post">
     <center>
-    <div style="display:flex">
-        <div class="karta">
-            
-                <p>podaj numer karty</p>
-                <input name="kartaNumer" type="number">
-                <input type="checkbox" name="karta" id="">
-        </div>
-        <div class="przelew">
-            <form action="zamowienia.php" method="post">
-                <p>podaj dane konta</p>
-                <input name="kontoNumer" type="number">  
-                <input type="checkbox" name="Przelew" id="">
-            
-        </div>
+    <div class="nwm">
+        <a href="zamowienia.php?metoda=przelew">Przelew</a>
+        <a href="zamowienia.php?metoda=karta">Kartą</a>
     </div>
-    </center>
-    <center>
-        <div>
-            <h1>wybierz metode dostawy</h1>
-            <span>Kurier <input type="text" name="adres" placeholder="adres"></span>
-            <input type="checkbox" name="kurier" id="">
-        </div>
-    </center>
     </form>
 </body>
 </html>
